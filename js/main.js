66357ae6a2f9b40e979279c50fc99bd7e7b723a0
Winston Co <winstonco@live.com>
2022-09-06 02:15:04 -1000

    new animations, new scrollbar
    animation to lift intro screen, arrow rotation, color palette is reusable, new verticalSlide function

---
+++ js/main.js
@@ -0,0 +1,55 @@
+const triangle_wrapper = document.getElementById('triangle_wrapper');
+const intro_triangle = document.getElementById('intro_triangle');
+// if the intro screen is up
+var isUp = false;
+
+// check if null
+if (triangle_wrapper) {
+  triangle_wrapper.addEventListener('click', function() {
+    if (isUp == false) {
+      slideFromBottom('.intro_screen', -8, 95, 19, 1);
+      isUp = true;
+    } else {
+      slideFromBottom('.intro_screen', 87, 95, 19, -1);
+      isUp = false;
+    }
+  }, false);
+}
+
+/*
+ * Slides targetElement a given distance in vh in a given number of steps.
+ * The element's bottom position starts at initBottom in vh.
+ * Steps must be a factor of distance.
+ * Direction specified in 1:up or -1:down
+*/
+function slideFromBottom(targetElement, initBottom, distance, steps, direction) {
+  let target = document.querySelector(targetElement);
+  let id = null;
+  let pos = initBottom;
+  clearInterval(id);
+  id = setInterval(frame, 5);
+  // Different starting position based on direction
+  function frame() {
+    // Different target pos based on direction
+    if (pos == initBottom + distance * direction) {
+      clearInterval(id);
+    } else {
+      pos = pos + Math.round(distance/steps) * direction;
+      target.style.bottom = pos + 'vh';
+    }
+  }
+}
+
+// direction of arrow on intro screen
+let pointingDown = false;
+
+triangle_wrapper.addEventListener('click', flipTriangle);
+function flipTriangle() {
+  if (pointingDown) {
+    intro_triangle.classList.add('pointUp');
+    pointingDown = false;
+  } else {
+    intro_triangle.classList.remove('pointUp')
+    pointingDown = true;
+  }
+}
