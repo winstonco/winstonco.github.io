d39e5e07edab1d2448bbe9bceed9795908fc5f72
Winston Co <winstonco@live.com>
2023-10-29 03:50:40 -1000

    Updates

---
+++ assets/index-95811c23.js
@@ -0,0 +1,1 @@
+import{c as t,A as e,C as o,t as n}from"./index-f3b60b29.js";const r=n("<h1>Page Not Found"),c=()=>t(o,{get children(){return[r(),t(e,{href:"/",children:"Back to home"})]}});export{c as default};
